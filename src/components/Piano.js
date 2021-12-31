096ac7573d6656ca19c9ecc5baf83a1d482ee6c4
Juan Bernal <juanbernalcode@gmail.com>
2021-12-31 17:59:27 -0500

    Piano Crazy Keys git add .

--- src/components/Piano.js
+++ src/components/Piano.js
@@ -1,5 +1,5 @@
-
 import { p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23 } from './PianoSvg';
+
 
 const paths = [p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15,p16,p17,p18,p19,p20,p21,p22,p23]
 
